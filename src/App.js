843c75512fd9a1e97a8b487622467de445379e00
RiverO2442 <nguyem.anhhao.840@gmail.com>
2025-02-05 15:29:46 +0000

    update: add basic header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './pages/header/header';
+import Footer from './pages/footer/footer';
+import Pages from './pages';
 
 function App() {
   return (
     <div className="App bg-gray-600">
       <Header/>
+      <Pages/>
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +21,7 @@ function App() {
         >
           Learn React
         </a> */}
+        <Footer/>
     </div>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div class="bg-white min-w-[100vw] min-h-[100vh]">Pages</div>
+  )
+}
+
+export default Pages
