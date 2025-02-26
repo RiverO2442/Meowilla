153c10b35691d7e8d3d8544d79b6cac2d502d010
haona <hao.na@tasco.com.vn>
2025-02-26 20:31:20 +0700

    Code migration to Vite based

--- src/pages/media/media.jsx
+++ src/pages/media/media.jsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import logo from "./img/logo.png";
 import "./styles.css";
 
 export default function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800 footer-container">
+    <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800 footer-container">
       <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
