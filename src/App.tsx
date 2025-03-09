45baca2d98e2c2ad246d1a2a4e40919ecc98f808
haona <hao.na@tasco.com.vn>
2025-03-10 04:50:41 +0700

    feat: add footer and new img detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./pages/header/header";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Media from "./pages/media/media";
+import Footer from "./pages/footer/footer";
+import ImageDetail from "./pages/media/media-detail/media-detail";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div className="App bg-gray-600">
-        {/* <Header />
-        <Pages />
-        <Footer /> */}
-      </div>
-    </>
+    <Router>
+      <Box
+        display="flex"
+        flexDirection="column"
+        minHeight="100vh"
+        width={"100%"}
+      >
+        <Header />
+        <div className="p-3 flex flex-row bg-white min-h-[1000px]">
+          <Routes>
+            <Route path="/" element={<Media />} />
+            <Route path="/detail/:id" element={<ImageDetail />} />
+            {/* <Route path="/about" element={<About />} /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </Box>
+    </Router>
   );
-}
+};
 
 export default App;
